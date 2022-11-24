be0d5ab689f1a507bc08dec37c14c0b37d4fd94f
TMDStudios <tmdstudioscontact@gmail.com>
2022-11-23 20:42:54 -0500

    Updated title effect

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,6 +1,7 @@
 var welcome = "Welcome to Game Room".split('');
 var welcomeIndex = 0;
 var scrollRight = false;
+var lastIndex = 0;
 
 $(document).ready(function() {
 	for(var i=0; i<welcome.length; i++){
@@ -12,9 +13,12 @@ $(document).ready(function() {
 function handleColors(){
 	scrollRight ? welcomeIndex-- : welcomeIndex++;
     if(welcomeIndex<=0||welcomeIndex>=welcome.length-1){scrollRight=!scrollRight;}
+    if(welcomeIndex>0&&scrollRight){lastIndex=welcomeIndex-1;}
+    else if(welcomeIndex<welcome.length-1&&!scrollRight){lastIndex=welcomeIndex+1;}
 	var r = Math.floor(Math.random() * 155 + 100);
 	var g = Math.floor(Math.random() * 155 + 100);
 	var b = Math.floor(Math.random() * 155 + 100);
-	document.getElementById(welcomeIndex).style.color = 'rgba(' + r + ',' + g + ',' + b + ',' + 1 + ')';
-    setTimeout(handleColors, 75);
+	document.getElementById(welcomeIndex).style = 'color: rgba(' + r + ',' + g + ',' + b + ',' + 1 + '); font-size: 1em';
+	document.getElementById(lastIndex).style = 'color: rgba(255, 255, 255, 1); font-size: 1.1em';
+    setTimeout(handleColors, 100);
 }

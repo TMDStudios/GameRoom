7dd3c2d3b0ccdd79f4fb4b03434d8d6e6f28610c
TMDStudios <tmdstudioscontact@gmail.com>
2022-12-03 14:38:42 -0500

    Updated title animation

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,12 +1,12 @@
 var welcome = "Welcome to Game Room".split('');
 var welcomeIndex = 0;
-var scrollRight = false;
-var lastIndex = 0;
+var b = 0;
 
 $(document).ready(function() {
 	if(document.getElementById("welcome")!=null){
 		for(var i=0; i<welcome.length; i++){
-			$("#welcome").append("<span id='"+i+"' style='color: rgba(255, 255, 255, 1);'>" + welcome[i] + "</span>");
+			b+=10;
+			$("#welcome").append("<span id='"+i+"' style='color: rgba(0, 0, "+b+", 1);'>" + welcome[i] + "</span>");
 		}
 		handleColors();
 	}
@@ -16,16 +16,9 @@ $(document).ready(function() {
 });
 
 function handleColors(){
-	scrollRight ? welcomeIndex-- : welcomeIndex++;
-    if(welcomeIndex<=0||welcomeIndex>=welcome.length-1){scrollRight=!scrollRight;}
-    if(welcomeIndex>0&&scrollRight){lastIndex=welcomeIndex-1;}
-    else if(welcomeIndex<welcome.length-1&&!scrollRight){lastIndex=welcomeIndex+1;}
-	var r = Math.floor(Math.random() * 155 + 100);
-	var g = Math.floor(Math.random() * 155 + 100);
-	var b = Math.floor(Math.random() * 155 + 100);
-	document.getElementById(welcomeIndex).style = 'color: rgba(' + r + ',' + g + ',' + b + ',' + 1 + '); font-size: 1em';
-	document.getElementById(lastIndex).style = 'color: rgba(255, 255, 255, 1); font-size: 1.1em';
-    setTimeout(handleColors, 100);
+	document.getElementById(welcomeIndex).style = 'color: rgba(255, 255, 255, 1);';
+	welcomeIndex++;
+    if(welcomeIndex<welcome.length){setTimeout(handleColors, 10);}
 }
 
 function showWarning(){
